e05a26012ab4c191926f07f46441afbce0bcdb6d
haneenarukhiya <haneenarukhiya@gmail.com>
2022-08-23 09:51:33 +0530

    heroku 555 changed

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .import views
 urlpatterns=[
-path("my first page",views.homeview,name="fist"),
+path(" ",views.homeview,name="fist"),
 path("mysecondpage",views.second,name="second"),
 path("extention", views.extention,name="extent"),
 path("secondpage",views.homeview,name="second"),
--- firstproject/urls.py
+++ firstproject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("firstapp/",include('firstapp.urls'))
+    path(" ",include('firstapp.urls'))
 ]
